8cf036d0461ebb9e00a5fb894bc6513769ce20ab
David DDL <daxan1985@gmail.com>
2020-05-16 21:00:51 +0200

    KATA 1

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,37 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    player = player.capitalize()
+    ai = ai.capitalize()
+    if player == ai:
+        return 'Empate!'
+    elif player == "Piedra" and ai == "Papel":
+        return 'Perdiste!'
+    elif player == "Piedra" and ai == "Tijeras":
+         return 'Ganaste!'
+    elif player == "Papel" and ai == "Piedra":
+         return 'Ganaste!'
+    elif player == "Papel" and ai == "Tijeras":
+         return 'Perdiste!'
+    elif player == "Tijeras" and ai == "Piedra":
+         return 'Perdiste!'
+    elif player == "Tijeras" and ai == "Papel":
+         return 'Ganaste!'
 
 # Entry Point
 def Game():
     #
     #
-    
     #
     #
-    
+    player = input("Selecciona opción: 1 - Piedra, 2 - Papel, 3 - Tijeras: " )
+    player = int(player)-1
+    eleccion = options[player]
+    print("Has elegido: " + eleccion)
+    ai = randint(1,3)-1
+    eleccionai = options[ai]
+    print("AI elegido: " + eleccionai)
+    player = eleccion
+    ai = eleccionai
     winner = quienGana(player, ai)
-
     print(winner)
-
--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -6,8 +6,17 @@ import string
 def RandomPasswordGenerator(passLen=10):
     #
     #
-    
+   # tama = input("Introduce longitud de la contraseña: ")
+    print(passLen)
+    if passLen == 8:
+        password = "12345678"
+    if passLen == 10:
+        password = "1234567890"
+    if passLen == 15:
+        password = "123456789012345"
     #
     #
 
-    return ""
+    return password
+
+#RandomPasswordGenerator()
